d6124f7ffd6c5a6d020dd90ccda0719691a0cc65
techtek <smaris@gmail.com>
2018-05-09 20:13:51 +0800

    Blinkit v2.1 - Arduino

---
+++ ArduinoConsoleForm.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ArduinoConsoleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ArduinoConsoleForm))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.SerialText = New System.Windows.Forms.RichTextBox()
+        Me.txtSend = New System.Windows.Forms.TextBox()
+        Me.BtnSend = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Blinkit.My.Resources.Resources.LogoArduino
+        Me.PictureBox1.Location = New System.Drawing.Point(799, 56)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(231, 183)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 16
+        Me.PictureBox1.TabStop = False
+        '
+        'SerialText
+        '
+        Me.SerialText.Location = New System.Drawing.Point(50, 90)
+        Me.SerialText.Name = "SerialText"
+        Me.SerialText.Size = New System.Drawing.Size(710, 148)
+        Me.SerialText.TabIndex = 15
+        Me.SerialText.Text = ""
+        '
+        'txtSend
+        '
+        Me.txtSend.Location = New System.Drawing.Point(50, 55)
+        Me.txtSend.Name = "txtSend"
+        Me.txtSend.Size = New System.Drawing.Size(634, 22)
+        Me.txtSend.TabIndex = 14
+        '
+        'BtnSend
+        '
+        Me.BtnSend.Location = New System.Drawing.Point(685, 55)
+        Me.BtnSend.Name = "BtnSend"
+        Me.BtnSend.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSend.TabIndex = 13
+        Me.BtnSend.Text = "Send"
+        Me.BtnSend.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Crimson
+        Me.Button3.Font = New System.Drawing.Font("Myriad Pro", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(230, 250)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 52)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Post"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.Button1.Font = New System.Drawing.Font("Myriad Pro", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(140, 250)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 52)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Follower"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.ForestGreen
+        Me.Button2.Font = New System.Drawing.Font("Myriad Pro", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(50, 250)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 52)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "upvote"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'ArduinoConsoleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Blinkit.My.Resources.Resources.backround_051
+        Me.ClientSize = New System.Drawing.Size(1072, 354)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.SerialText)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.txtSend)
+        Me.Controls.Add(Me.BtnSend)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "ArduinoConsoleForm"
+        Me.Text = "Blikit - Arduino Console"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents SerialText As RichTextBox
+    Friend WithEvents txtSend As TextBox
+    Friend WithEvents BtnSend As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ ArduinoConsoleForm.vb
@@ -0,0 +1,113 @@
+﻿Imports System.IO
+
+Public Class ArduinoConsoleForm
+
+    Private Sub arduinoconsole_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Sub New()
+        InitializeComponent()
+    End Sub
+
+    Dim WithEvents SerialPort As New IO.Ports.SerialPort
+
+    Private Sub btnSend_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSend.Click
+        If SerialPort.IsOpen Then
+            SerialPort.Write(txtSend.Text)
+        Else
+            ConnectSerial()
+            SerialPort.Write(txtSend.Text)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ConnectSerial()
+    End Sub
+
+    Private Sub ConnectSerial()
+        Try
+            SerialPort.BaudRate = My.Computer.FileSystem.ReadAllText("config\arduinobaudrate.txt")
+            SerialPort.PortName = My.Computer.FileSystem.ReadAllText("config\arduinocomport.txt")
+            SerialPort.Open()
+        Catch
+            SerialPort.Close()
+        End Try
+    End Sub
+
+    Delegate Sub myMethodDelegate(ByVal [text] As String)
+    Dim myD1 As New myMethodDelegate(AddressOf myShowStringMethod)
+
+    'display the text from the arduino into textbox SerialText
+    Sub myShowStringMethod(ByVal myString As String)
+        SerialText.AppendText(myString)
+    End Sub
+
+    Private Sub SerialPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        Dim str As String = SerialPort.ReadExisting()
+        Invoke(myD1, str)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If SerialPort.IsOpen Then
+            SerialPort.Write("u")
+        Else
+            ConnectSerial()
+            SerialPort.Write("u")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If SerialPort.IsOpen Then
+            SerialPort.Write("f")
+        Else
+            ConnectSerial()
+            SerialPort.Write("f")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If SerialPort.IsOpen Then
+            SerialPort.Write("p")
+        Else
+            ConnectSerial()
+            SerialPort.Write("p")
+        End If
+    End Sub
+
+
+
+
+
+    Private Sub txtSend_TextChanged(sender As Object, e As EventArgs) Handles txtSend.TextChanged
+
+    End Sub
+
+    Private Sub SerialText_TextChanged(sender As Object, e As EventArgs) Handles SerialText.TextChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ArduinoConsoleForm_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        ArduinoForm.Show()
+        ArduinoForm.BringToFront()
+
+        SerialPort.Close()
+
+    End Sub
+
+
+End Class
